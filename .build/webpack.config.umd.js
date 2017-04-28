59d210011a5645e341d6743be14bf33fa98a85c0
Stanislav Sysoev <d4rkr00t@gmail.com>
2017-04-28 15:10:52 +1000

    build: Add UMD and CJS builds

---
+++ .build/webpack.config.umd.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src",
+  output: {
+    filename: "prosemirror-dev-tools.min.js",
+    path: path.resolve(__dirname, "..", "dist", "umd"),
+    library: "ProseMirrorDevTools",
+    libraryTarget: "umd"
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      output: { comments: false }
+    }),
+    new webpack.DefinePlugin({ "process.env.NODE_ENV": '"production"' })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
+};
