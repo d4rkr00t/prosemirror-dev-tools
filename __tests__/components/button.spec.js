510c696abc145fee276c32170b593ef5e6f9b898
ckrishnakumar <ckrishnakumar@atlassian.com>
2017-05-01 13:42:55 +1000

    update test syntax

--- __tests__/components/button.spec.js
+++ __tests__/components/button.spec.js
@@ -1,17 +1,15 @@
 import React from "react";
 import renderer from "react-test-renderer";
 
-describe('Button test',function(){
-  var Button = require('../../src/components/button').default
+ var Button = require('../../src/components/button').default
   var input = [{
     isActive: '1',
     theme: 'white05'
   }];
 
-  it('Button renders when active', () => {
+  test('Button renders when active', () => {
     var button = renderer.create(<Button
       params = {input}
     />).toJSON();
     expect(button).toMatchSnapshot();
   });
-})
