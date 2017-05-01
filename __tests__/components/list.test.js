999a06809843837b52c8234b6f2cb94fd4206f6a
ckrishnakumar <ckrishnakumar@atlassian.com>
2017-05-01 21:25:03 +1000

    test(Adding support for tests and some tests):

---
+++ __tests__/components/list.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+jest.dontMock("../../src/components/list");
+
+test.only("ListItemGroup renders when clicked", () => {
+  const module = require("../../src/components/list");
+  const component = module.List({
+    title: jest.fn(),
+    groupTitle: jest.fn(),
+    items: ["", "", ["", "", "", ""]]
+  });
+  let tree = renderer.create(component).toJSON();
+  expect(tree).toMatchSnapshot();
+});
