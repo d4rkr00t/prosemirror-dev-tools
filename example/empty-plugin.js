08fef8656aee7670f5a3b4e9786298034fbfbe54
Stanislav Sysoev <d4rkr00t@gmail.com>
2017-05-01 10:26:42 +1000

    fix: print "func" instead of full function body in plugin state view
    Closes: #32

---
+++ example/empty-plugin.js
@@ -0,0 +1,22 @@
+import { Plugin, PluginKey } from "prosemirror-state";
+
+function createPluginState() {
+  return {
+    doSomething() {}
+  };
+}
+
+const key = new PluginKey("empty-plugin");
+const plugin = new Plugin({
+  key,
+  state: {
+    init() {
+      return createPluginState();
+    },
+    apply(tr, pluginState) {
+      return pluginState;
+    }
+  }
+});
+
+export default plugin;
