4186503e474db257410aa296ed86de2fa6ce42d6
Yangfan Zhang <yzhang2@atlassian.com>
2024-03-21 22:18:52 +1000

    sort plugin

--- src/tabs/plugins.tsx
+++ src/tabs/plugins.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import type { Plugin } from "prosemirror-state";
 import { InfoPanel } from "../components/info-panel";
 import { Heading } from "../components/heading";
 import JSONTree from "../components/json-tree";
 import { List } from "../components/list";
 import { SplitView, SplitViewCol } from "../components/split-view";
-import { atom, useAtom, useAtomValue } from "jotai";
+import { useAtomValue } from "jotai";
 import { editorStateAtom } from "../state/editor-state";
+import SearchBar from "../components/search-bar";
+import Button from "../components/button";
 
 export function valueRenderer(raw: string, ...rest: Array<string>) {
   if (typeof rest[0] === "function") {
@@ -19,40 +21,90 @@ export function PluginState(props: { pluginState: any }) {
   return (
     <div>
       <Heading>Plugin State</Heading>
-      <JSONTree data={props.pluginState} valueRenderer={valueRenderer} />
+      <JSONTree
+        data={props.pluginState}
+        valueRenderer={valueRenderer}
+        sortObjectKeys={true}
+      />
     </div>
   );
 }
 
-const selectedPluginIndexAtom = atom(0);
-
 // TODO: replace isDimmed with useCallback once EditorStateContainer is decomposed
 export default function PluginsTab() {
-  const [selectedIndex, setSelected] = useAtom(selectedPluginIndexAtom);
-  const isSelected = React.useCallback(
-    (_plugin, index) => selectedIndex === index,
-    [selectedIndex]
-  );
-  const handleOnListItemClick = React.useCallback(
-    (_plugin, index) => setSelected(index),
-    []
-  );
   const state = useAtomValue(editorStateAtom);
-
   if (!state) return null;
 
-  const plugins = state.plugins as any as Plugin<any>[];
-  const selectedPlugin = plugins[selectedIndex];
+  const [selectedPlugin, setSelectedPlugin] = useState(state.plugins[0]);
+  const [pluginsLocal, setPluginsLocal] = useState(state.plugins);
+  const [sortAsc, setSortOrder] = useState(true);
+
+  const handleOnListItemClick = React.useCallback(
+    (_plugin) => setSelectedPlugin(_plugin),
+    []
+  );
+
   const selectedPluginState = selectedPlugin.getState(state);
+
+  const handleSearch = (input: string) => {
+    const filteredPlugins = (state.plugins as any as Plugin<any>[]).filter(
+      (plugin) => {
+        return (plugin as any).key.toLowerCase().includes(input.toLowerCase());
+      }
+    );
+    setPluginsLocal(filteredPlugins);
+  };
+
+  const handleClickSort = () => {
+    setSortOrder(!sortAsc);
+  };
+
+  const handleSortAsc = (plugins: any) => {
+    return [...plugins].sort((a, b) => {
+      if ((a as any).key < (b as any).key) {
+        return -1;
+      }
+      if ((a as any).key > (b as any).key) {
+        return 1;
+      }
+      return 0;
+    });
+  };
+  const handleSortDes = (plugins: any) => {
+    return [...plugins].sort((a, b) => {
+      if ((a as any).key < (b as any).key) {
+        return 1;
+      }
+      if ((a as any).key > (b as any).key) {
+        return -1;
+      }
+      return 0;
+    });
+  };
 
   return (
     <SplitView testId="__prosemirror_devtools_tabs_plugins__">
       <SplitViewCol noPaddings>
+        <div
+          style={{
+            display: "flex",
+            marginLeft: 6,
+            marginRight: 6,
+            marginBottom: 3,
+            marginTop: 3,
+          }}
+        >
+          <SearchBar onSearch={handleSearch} />
+          <Button onClick={handleClickSort}>
+            SORT {sortAsc ? "DES" : "ASC"}
+          </Button>
+        </div>
         <List
-          items={plugins}
+          items={
+            sortAsc ? handleSortAsc(pluginsLocal) : handleSortDes(pluginsLocal)
+          }
           getKey={(plugin: Plugin) => (plugin as any).key}
           title={(plugin: Plugin) => (plugin as any).key}
-          isSelected={isSelected}
           isDimmed={(plugin: Plugin) => !plugin.getState(state)}
           onListItemClick={handleOnListItemClick}
         />
