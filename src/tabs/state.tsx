2ba9bad44a312ca64ec246a489c593b94fcadb93
Stanislav Sysoev <d4rkr00t@gmail.com>
2026-02-08 12:08:37 +1100

    chore: fix types after jsonpatchdiff update

--- src/tabs/state.tsx
+++ src/tabs/state.tsx
@@ -7,11 +7,7 @@ import {
 } from "../utils/format-selection-object";
 import { SplitView, SplitViewCol } from "../components/split-view";
 import JSONTree from "../components/json-tree";
-import {
-  Heading,
-  HeadingWithButton,
-  HeadingButton,
-} from "../components/heading";
+import { Heading, HeadingWithButton, HeadingButton } from "../components/heading";
 import theme from "../theme";
 import { activeMarksAtom } from "../state/active-marks";
 import { expandPathAtom } from "../state/expand-path";
@@ -125,7 +121,7 @@ export function getItemString(
   ) {
     const logButton = (
       <LogNodeButton
-        onClick={(e) => {
+        onClick={(e: MouseEvent) => {
           e.preventDefault();
           e.stopPropagation();
           action({ doc, node: value });
@@ -213,15 +209,13 @@ export default function StateTab() {
       <SplitViewCol grow>
         <HeadingWithButton>
           <Heading>Current Doc</Heading>
-          <HeadingButton onClick={() => console.log(state)}>
-            Log State
-          </HeadingButton>
+          <HeadingButton onClick={() => console.log(state)}>Log State</HeadingButton>
         </HeadingWithButton>
         <JSONTree
           data={doc}
           hideRoot
           getItemString={getItemString(doc, logNode)}
-          shouldExpandNode={(nodePath) =>
+          shouldExpandNode={(nodePath: Array<string | number>) =>
             shouldExpandNode(expandPath, nodePath)
           }
         />
@@ -249,11 +243,7 @@ export default function StateTab() {
           <Heading>Active Marks</Heading>
           <JSONTreeWrapper>
             {activeMarks.length ? (
-              <JSONTree
-                data={activeMarks}
-                hideRoot
-                getItemString={getItemStringForMark}
-              />
+              <JSONTree data={activeMarks} hideRoot getItemString={getItemStringForMark} />
             ) : (
               <Group>
                 <GroupRow>
